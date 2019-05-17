a29895dd8b79e5d0204a8fb99260e9499395eae7
Bruno Gama <hello@xuxu.rocks>
2019-05-17 18:50:33 -0300

    Set monitor color to rgb full

--- screenlayouts/work.sh
+++ screenlayouts/work.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-xrandr --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 266x1080 --rotate normal
+xrandr --output HDMI-1 \
+       --mode 1920x1080 \
+       --pos 0x0 \
+       --rotate normal \
+       --set "BroadcastRGB" "Full"
+       --output eDP-1 \
+       --primary \
+       --mode 1366x768 \
+       --pos 266x1080 \
+       --rotate normal \
