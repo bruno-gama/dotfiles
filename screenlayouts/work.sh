3dae3ded486df7b88977969437998fdba866181d
Bruno Gama <hello@xuxu.rocks>
2019-05-20 11:04:25 -0300

    fix: Break lines correctly

--- screenlayouts/work.sh
+++ screenlayouts/work.sh
@@ -3,7 +3,7 @@ xrandr --output HDMI-1 \
        --mode 1920x1080 \
        --pos 0x0 \
        --rotate normal \
-       --set "BroadcastRGB" "Full"
+       --set "BroadcastRGB" "Full" \
        --output eDP-1 \
        --primary \
        --mode 1366x768 \
