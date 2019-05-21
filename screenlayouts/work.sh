f053224528a0c39bfdc27a298a7f3267bd797192
Bruno Gama <hello@xuxu.rocks>
2019-05-21 11:20:43 -0300

    fix: Set Broadcast RGB correctly

--- screenlayouts/work.sh
+++ screenlayouts/work.sh
@@ -3,9 +3,10 @@ xrandr --output HDMI-1 \
        --mode 1920x1080 \
        --pos 0x0 \
        --rotate normal \
-       --set "BroadcastRGB" "Full" \
+       --set "Broadcast RGB" "Full" \
        --output eDP-1 \
        --primary \
        --mode 1366x768 \
        --pos 266x1080 \
-       --rotate normal \
+       --rotate normal
+
